9868ac591a4313cf7b23c81e029df617d1804d1d
jeibbiej <57351918+jeibbiej@users.noreply.github.com>
2021-08-11 22:15:26 +0800

    localStorage

--- js/fxn.js
+++ js/fxn.js
@@ -1,43 +1,52 @@
+function inArray(haystack, needle)
+{
+	// parameter check
+	if (!(haystack instanceof Array))
+		return -1;
+	if (typeof (haystack[0]) != typeof (needle))
+		return -1;
 
-function inArray(haystack, needle) {
-    // parameter check
-    if (!(haystack instanceof Array))
-        return -1;
-    if (typeof (haystack[0]) != typeof (needle))
-        return -1;
-
-    var len = haystack.length;
-    for (var i = 0; i < len; i++) {
-        if (haystack[i] == needle) {
-            return i;
-        }
-    }
-    //console.log("needle:" + needle + " haystack:" + haystack);
-    return -1
+	var len = haystack.length;
+	for (var i = 0; i < len; i++)
+	{
+		if (haystack[i] == needle)
+		{
+			return i;
+		}
+	}
+	//console.log("needle:" + needle + " haystack:" + haystack);
+	return -1
 };
 
-function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split("=");
-        if (pair[0] == variable)
-            return pair[1];
-    }
-    return "";
+function getQueryVariable(variable)
+{
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i = 0; i < vars.length; i++)
+	{
+		var pair = vars[i].split("=");
+		if (pair[0] == variable)
+			return pair[1];
+	}
+	return "";
 };
 
-function imgPokemon_Clicked(el, game) {
-    // check if image is "enabled" or not
-    if (el.classList.contains('gray')) {
-        el.classList.remove('gray');
-        deleteZukan(game, el.id);
-    }
-    else {
-        el.classList.add('gray');
-        insertZukan(game, el.id);
-    }
+function imgPokemon_Clicked(el, game)
+{
+	// check if image is "enabled" or not
+	if (el.classList.contains('gray'))
+	{
+		el.classList.remove('gray');
+		insertZukan(game, el.id);
+	}
+	else
+	{
+		el.classList.add('gray');
+		deleteZukan(game, el.id);
+	}
 }
+
+const lsName = "zukanData";
 
 /// <summary>
 /// Inserts a clicked "Pokémon" to the current game list.
@@ -45,10 +54,16 @@ function imgPokemon_Clicked(el, game) {
 /// <param name=""></param>
 /// <param name=""></param>
 /// <returns></returns>
-function insertZukan(game, pkmn) {
-    var zukan = localStorage.getObject("zukanData");
-    var zukanGame = zukan[game];
-    zukanGame.push(pkmn);
+function insertZukan(game, pkmn)
+{
+	var zukan = localStorage.getObject(lsName);
+	if (zukan == null)
+		zukan = {};
+	if (!zukan.hasOwnProperty(game))
+		zukan[game] = [];
+	if (!zukan[game].includes(pkmn))
+		zukan[game].push(pkmn);
+	localStorage.setObject(lsName, zukan);
 }
 
 /// <summary>
@@ -57,15 +72,28 @@ function insertZukan(game, pkmn) {
 /// <param name=""></param>
 /// <param name=""></param>
 /// <returns></returns>
-function deleteZukan(game, pkmn) {
-
+function deleteZukan(game, pkmn)
+{
+	var zukan = localStorage.getObject(lsName);
+	if (zukan == null)
+		return;
+	if (!zukan.hasOwnProperty(game))
+		return;
+	var idx = zukan[game].indexOf(pkmn);
+	if (idx >= 0)
+		delete zukan[game][idx];
+	localStorage.setObject(lsName);
 }
 
-Storage.prototype.setObject = function (key, value) {
-    this.setItem(key, JSON.stringify(value));
+Storage.prototype.setObject = function (key, value)
+{
+	this.setItem(key, JSON.stringify(value));
 }
 
-Storage.prototype.getObject = function (key) {
-    var val = this.getItem(key);
-    return value && JSON.parse(val);
+Storage.prototype.getObject = function (key)
+{
+	var val = this.getItem(key);
+	if (val === 'undefined')
+		return {};
+	return val && JSON.parse(val);
 }
