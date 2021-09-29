ec708a70433c05fbf110bf7474866e1e7c910fad
jeibbiej <57351918+jeibbiej@users.noreply.github.com>
2021-09-29 09:51:46 +0800

    Create go-events.js

---
+++ js/go-events.js
@@ -0,0 +1,12 @@
+//=============================================================
+// List of pokemon variants release dates (and availability).
+// https://www.serebii.net/pokemongo/events.shtml
+//=============================================================
+const _GO_Events =
+[
+    {name: "Halloween 2016",    begDate: "2016-10-26", endDate: "2016-11-01"},
+    {name: "Thanksgiving 2016", begDate: "2016-11-23", endDate: "2016-11-30"},
+/*
+    {name: "", begDate: "", endDate: ""},
+*/
+]
